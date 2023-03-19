5f96a25771af11f18d938999697b9142a1dbccd9
artem <artempolevoj@yandex.ru>
2023-03-19 09:28:09 +0300

    Addeted exeercise 2

--- Training_python/seminar3.py
+++ Training_python/seminar3.py
@@ -111,3 +111,30 @@ elif 9 <= number_mounth <= 11:
     print(f'Результат через словарь: {time_year_dist.get(3)}')
 else:
     print("Нет такого мясяца")
+"""
+2. Реализовать функцию, принимающую два числа (позиционные аргументы)
+и выполняющую их деление. Числа запрашивать у пользователя,
+предусмотреть обработку ситуации деления на ноль (try except).
+Пример:
+Введите первое число: 10
+Введите второе число: 0
+Вы что? Пытаетесь делить на 0!
+Process finished with exit code 0
+Пример:
+Введите первое число: 10
+Введите второе число: 10
+1.0
+
+Process finished with exit code 0
+"""
+def division(a, b):
+    try:
+        print(a / b)
+    except ZeroDivisionError:
+        print("Вы что? Пытаетесь делить на 0!")
+    finally:
+        print("Process finished with exit code 0")
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+division(a, b)
