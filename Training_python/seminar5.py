e1ea504e72108d16528449c1be5075fc8dbaf7e5
Артём <artempolevoj@yandex.ru>
2023-03-20 19:31:34 +0300

    Addeted exeercise 28

--- Training_python/seminar5.py
+++ Training_python/seminar5.py
@@ -28,3 +28,32 @@ number = get_number('Введите число: ')
 degree = get_number('Введите степень: ')
 result = get_degree(number, degree)
 print(f'{number} в степени {degree} = {result}')
+"""
+Задача 28:
+Напишите рекурсивную функцию sum(a, b),
+возвращающую сумму двух целых неотрицательных чисел. Из
+всех арифметических операций допускаются только +1 и -1.
+Также нельзя использовать циклы.
+"""
+def get_number(string):
+    global num
+    try:
+        num = int(input(string))
+    except ValueError:
+        print('Введено не число')
+        get_number(string)
+    if num < 0:
+        print('Введено отрицательное число')
+        get_number(string)
+    return num
+
+def get_sum(a, b):
+    if b == 0:
+        return a
+    else:
+        return get_sum(a + 1, b - 1)
+
+num_A = get_number('Введите целое не отрицательное число A: ')
+num_B = get_number('Введите целое не отрицательное число B: ')
+result = get_sum(num_A, num_B)
+print(f'{num_A} + {num_B} = {result}')
