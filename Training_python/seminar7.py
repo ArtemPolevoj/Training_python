9fb8c9ca34a56273b908bf3244e3a188f847e28c
Артём <artempolevoj@yandex.ru>
2023-04-03 10:50:36 +0300

    Addeted task 3

--- Training_python/seminar7.py
+++ Training_python/seminar7.py
@@ -64,4 +64,54 @@ road = Road(20, 5000)
 result = road.weight(25, 0.05)
 print(f'{result / 1000} тонн')
 
+"""
+Задание 3.
+Реализовать базовый класс Worker (работник),
+в котором определить публичные атрибуты name, surname, position (должность),
+и защищенный атрибут income (доход). Последний атрибут должен ссылаться
+на словарь, содержащий элементы: оклад и премия, например,
+{"wage": wage, "bonus": bonus}.
+Создать класс Position (должность) на базе класса Worker. В классе Position
+реализовать публичные методы
+получения полного имени сотрудника (get_full_name) и дохода с учетом
+премии (get_total_income).
+Проверить работу примера на реальных данных (создать экземпляры класса Position,
+передать данные,
+проверить значения атрибутов, вызвать методы экземпляров).
+П.С. попытайтесь добить вывода информации о сотруднике также через
+перегрузку str
+str(self) - вызывается функциями str, print и format. Возвращает строковое
+представление объекта.
+"""
 
+
+class Worker:
+    name = ''
+    surname = ''
+    position = ''
+    _income = {}
+
+
+class Position(Worker):
+
+    def __init__(self, wage, bonus):
+        self._income["wage"] = wage
+        self._income["bonus"] = bonus
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return sum(self._income.values())
+
+    def __str__(self):
+        return f'{self.name} {self.surname}, оклад = {self._income["wage"]},' \
+               f' премия = {self._income["bonus"]}'
+
+
+engineer = Position(5000, 1000)
+engineer.name = "Василий"
+engineer.surname = "Иванов"
+print(engineer.get_full_name())
+print(engineer.get_total_income())
+print(engineer)
